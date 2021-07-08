f4a01eaa1e744c43db733aca18019ec9aa3b335a
Juliana Pereira <jotace1234@hotmail.com>
2021-07-08 08:39:56 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Naming the players:
 ////// 1) using the hardcoding method (to be akcnowledged, not to be used)
@@ -13,14 +13,14 @@ document.querySelector('#name--1').textContent = playerTwo;
 
 let listPlayers = [];
 
-const playersNames = function (nbrPlayers) {
+const playersNamesInput = function (nbrPlayers) {
   for (let i = 0; i < nbrPlayers; i++) {
     listPlayers[i] = prompt(`What's the name of Player ${i + 1}?`);
   }
   return listPlayers;
 };
 
-playersNames(2); //In this particular game, it'll always be 2 players
+playersNamesInput(2); //In this particular game, it'll always be 2 players
 
 /* FIND BELOW THE FUNCTION TO BE USED IN CASE YOU WANT THE USER TO TELL YOU HOW MANY PLAYERS ARE GOING TO PLAY PROVIDED THAT, OF COURSE, YOUR GAME ALLOWS 2+ PLAYERS:
 
@@ -34,24 +34,28 @@ const numberPlayers = function () {
 numberPlayers();
 */
 
-let playerName = document.querySelectorAll('.name');
+let playerName = document.querySelectorAll(".name");
 
-for (let i = 0; i < listPlayers.length; i++) {
-  playerName[i].textContent = listPlayers[i];
-}
+const playersNamePrint = function () {
+  for (let i = 0; i < listPlayers.length; i++) {
+    playerName[i].textContent = listPlayers[i];
+  }
+};
+
+playersNamePrint();
 
 //Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const current0El = document.querySelector('#current--0');
-const score1El = document.querySelector('#score--1');
-const current1El = document.querySelector('#current--1');
-let currentScoreBoth = document.querySelectorAll('.current-score');
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const current0El = document.querySelector("#current--0");
+const score1El = document.querySelector("#score--1");
+const current1El = document.querySelector("#current--1");
+let currentScoreBoth = document.querySelectorAll(".current-score");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 //Initial conditions variables
 let currentScore, activePlayer, playing, scores;
@@ -59,7 +63,7 @@ let currentScore, activePlayer, playing, scores;
 const gameStart = function () {
   score0El.textContent = 0;
   score1El.textContent = 0;
-  diceEl.classList.add('hidden');
+  diceEl.classList.add("hidden");
 
   for (let i = 0; i < currentScoreBoth.length; i++) {
     currentScoreBoth[i].textContent = 0;
@@ -70,10 +74,12 @@ const gameStart = function () {
   activePlayer = 0;
   playing = true;
 
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
 
-  player0El.classList.add('player--active');
+  player0El.classList.add("player--active");
+
+  playersNamePrint();
 };
 
 gameStart();
@@ -88,8 +94,8 @@ const playerSwitch = function () {
   //In other words: is the activePlayer "0"? Yes? So Switch to "1". No? So switch do "0"
   currentScore = 0;
   //Toggling the class .active--player ⬇️
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 
   // minha sugestão de como zerar os scores qd muda o player ⬇️
   for (let i = 0; i < currentScoreBoth.length; i++) {
@@ -98,12 +104,12 @@ const playerSwitch = function () {
 };
 
 //Rolling dice functionality
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     //1. Generate a random number (dice roll)
     const dice = Math.trunc(Math.random() * 6) + 1;
     //2. Display the dice image according to the number outcome
-    diceEl.classList.remove('hidden');
+    diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
     //3. Check if number 1 was outcome; if TRUE, switch to the other player
     if (dice !== 1) {
@@ -118,24 +124,27 @@ btnRoll.addEventListener('click', function () {
 });
 
 //Holding score functionality
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     // 1. add current score to the active player's global score
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. check if the active player's score is < 100
-    if (scores[activePlayer] < 100) {
+    if (scores[activePlayer] < 20) {
       playerSwitch();
     } else {
       playing = false;
-      diceEl.classList.add('hidden');
+      diceEl.classList.add("hidden");
+      playerName[
+        activePlayer
+      ].textContent = `${playerName[activePlayer].textContent} WINS!`;
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
+        .classList.add("player--winner");
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+        .classList.remove("player--active");
     }
   }
 });
@@ -148,4 +157,4 @@ btnNew.addEventListener('click', function () {
 });
 */
 ////Suggestion #2 -
-btnNew.addEventListener('click', gameStart);
+btnNew.addEventListener("click", gameStart);
